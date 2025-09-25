462bab7079eda422985d0a9d380fa2f2a18a0bf3
Anurag Mendhe <146920731+Anurag-Mendhe@users.noreply.github.com>
2025-09-25 11:50:58 +0530

    Add files via upload

---
+++ Numeric/numerical_data_type.py
@@ -0,0 +1,31 @@
+#numerical
+x = 5           # x is a variable that stores integers
+
+price = 26.25   # price is a float variable
+print(price)
+
+num = 3e6       # 3 into 10 raise to 6 
+                # e is 10     e is exponent number
+print(num)           
+
+#complex number
+z = 4 + 5j      #here j is used as complex number not i
+print(z)
+
+#tells the type of data type used
+print(type(z))
+print(type(num))
+print(type(x))
+
+print(abs(-5))   # in-build function for absolute value
+
+print(pow(2,3))  # in-build function for power
+
+print(round(4.75))  # rounds-off the number to the nearest integer 
+
+
+nums = [3,2,4,5,6]
+
+print(min(nums))    # in-build function for min 
+print(max(nums))    # in-build function for max
+print(sum(nums))    # in-build function for sum
